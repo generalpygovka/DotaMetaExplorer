9c484fe553218f15332a6b712ee9aef9bf4dfe63
Алексей Журавлёв <oleksejzuravlev@gmail.com>
2025-06-04 19:54:46 +0300

    откатился обратно, CI перестала работать из за того что не видит Constants

---
+++ DotaMetaExplorer/Constants.cs
@@ -0,0 +1,15 @@
+﻿using static System.Net.WebRequestMethods;
+
+namespace DotaMetaExplorer
+{
+    public class Constants
+    {
+
+        public static string heroes = "https://api.opendota.com/api/heroes";
+        public static string teams = "https://api.opendota.com/api/teams";
+        public static string players = "https://api.opendota.com/api/players";
+        public static string proPlayers = "https://api.opendota.com/api/proPlayers";
+        public static string proMatches = "https://api.opendota.com/api/proMatches";
+        public static string tokenApi = "?api_key=76b37873-3339-4684-a85b-d67c7605a573";
+    }
+}
